7da9e7dda831fc25ee26f4c5f1f36300a8612b84
Christina Beckford <cbeckford2@brooklynsteamcenter.org>
2023-04-17 14:52:43 -0400

    added a file

---
+++ grade.js
@@ -0,0 +1,19 @@
+let myGrade = 87
+if (myGrade <= 100 && myGrade >= 90){
+    console.log("Your letter grade is A")
+}
+if(myGrade <= 89 && myGrade >= 80){
+    console.log("Your letter grade is B")
+}
+if(myGrade <= 79 && myGrade >= 70){
+    console.log("Your letter grade is C")
+}
+if(myGrade <= 69 && myGrade >= 60){
+    console.log("Your letter grade is D")
+ }
+if (myGrade <= 59 && myGrade >= 50){
+    console.log("Your letter grade is F")
+}
+
+
+
---
+++ hello.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+let myName = "Christina"
+let myAge = 16
+console.log(myName)
+console.log(myAge)
+console.log("Hello my name is " + myName + ". My age is " + myAge )
+
+if (4 > 8) {
+    console.log("Math is incorrect")
+}
+if (4 < 8){
+    console.log ("Math is correct")
+}else{
+    console.log("Check math again")
+}
+
+let myList = [40, 7, 80]
+for (let item of myList){
+    console.log(item / 2)
+}
